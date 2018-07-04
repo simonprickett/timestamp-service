55597ad1e323a23c896fb7d35556d531775bdfee
Simon Prickett <simon@crudworks.org>
2018-07-04 14:44:37 -0700

    Initial commit.

---
+++ timestamp-service.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+console.log('Starting timestamp-service.')
+
+http.createServer((request, response) => {
+  const timeStamp = Math.floor(Date.now() / 1000)
+
+  console.log(`Received request at ${timeStamp}.`)
+
+  response.write(`${timeStamp}`)
+  response.end()
+}).listen(8000)
